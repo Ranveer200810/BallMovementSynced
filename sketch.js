49db4adf10dbef6e119d6340b9b946aa48ee33e6
Ranveer200810 <ranveerniras10@gmail.com>
2021-05-22 18:01:37 +0530

    RV: synchronised ball movement

--- sketch.js
+++ sketch.js
@@ -1,29 +1,54 @@
 var ball;
+var database, position;
 
 function setup(){
     createCanvas(500,500);
+
+    database = firebase.database();
+
+    var dbRef = database.ref("ball/position");
+    dbRef.on("value", readData);
+
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
 }
 
+function readData(data) {
+
+    position = data.val();
+
+}
+
 function draw(){
     background("white");
+
+    if (position !== undefined) {
+
+        ball.x = position.x;
+        ball.y = position.y;
+
+    }
+
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        updatePos(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        updatePos(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        updatePos(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        updatePos(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function updatePos(x,y){
+    database.ref("ball/position").update({
+
+        'x': position.x + x,
+        'y': position.y + y
+
+    });
 }
